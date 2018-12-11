30a4935cd65aba96a83ba9cf2994c536a52a0305
Haasi530 <haasithapidaparthi@10.250.118.53>
2018-12-10 18:55:52 -0800

    Updated Insert

--- KDTree/src/KDTree.java
+++ KDTree/src/KDTree.java
@@ -16,6 +16,44 @@ public class KDTree extends Node{
 		size = 0;
 	}
 	
+	// Insert 2D points into the tree
+	 public void insert(Point2D p) {
+	        if (p == null) 
+	        		return;
+	        root = insert(root, p);
+	 }
+	 private Node insert(Node parent, Point2D p) {
+		 // If parent is null
+		 if (parent == null) {
+			 Node node = new Node();
+			 node.p = p;
+			 node.left = null;
+			 node.right = null;
+			 size++;
+			 return node;
+		 }
+		 if (p.getX() < parent.p.getX()) {
+			 if(parent.left != null) {
+				 parent.left = insert(parent.left, p);
+				 return parent;
+			 } 
+			 parent.left = insert(parent.left, p);
+		 } else {
+				 if (parent.right != null) {
+					 parent.right = insert(parent.right, p);
+					 return parent;
+				 }
+				 parent.right = insert(parent.right, p);
+			 }
+		 } else {
+			 if (parent.right != null) {
+				 parent.right = insert(parent.right, p);
+				 return parent;
+			 }
+			 parent.right = insert(parent.right, p);
+			 return parent;
+		 }      
+	
 	public void search(){
 		if(root != null){
 			
