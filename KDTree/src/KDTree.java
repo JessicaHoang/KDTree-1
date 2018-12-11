96e4377f5cadb7c5e66e23e6e69116c97d08ad51
Haasi530 <haasithapidaparthi@10.250.118.53>
2018-12-10 18:13:11 -0800

    Basic Methods

--- KDTree/src/KDTree.java
+++ KDTree/src/KDTree.java
@@ -1,4 +1,28 @@
+import java.util.*;
+import java.awt.geom.Point2D;
 
 public class KDTree {
-
+	
+	// Represents direction of the partition
+	private static final boolean VERTICAL = true;
+    private static final boolean HORIZOTAL = false;
+	
+	private Node root;
+	private int size;
+	
+	// default empty tree
+	public KDTree() {
+		root = null;
+		size = 0;
+	}
+	
+	// Empty Method
+    public boolean isEmpty() {
+    		return root == null;
+    }
+    
+    // Number of nodes in the tree 
+    public int size() {
+        return size;
+    }
 }
