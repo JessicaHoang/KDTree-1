e7e4f8b4e549633a40214a0656eacfc6e7b3642c
Devanshi <sukhadia.devanshi@gmail.com>
2018-12-10 18:09:05 -0800

    updated node and KDTree class

--- KDTree/src/KDTree.java
+++ KDTree/src/KDTree.java
@@ -1,7 +1,16 @@
 
-public class KDTree {
+public class KDTree extends Node{
      
 	public void search(){
+		if(root != null){
+			
+		}
+		
+	}
+	
+	public <T> void contains(T value){
+		if(value == null || root == null)
+		return;
 		
 	}
 }
