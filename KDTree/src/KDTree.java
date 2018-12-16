0ec075f92c1568a2625a13a1c58865bb4acef02e
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2018-12-16 12:00:51 -0800

    Update KDTree.java
    Insert method was changed to check both x and y coordinates.
    comparePts method was included for insert method

--- KDTree/src/KDTree.java
+++ KDTree/src/KDTree.java
@@ -5,7 +5,7 @@ public class KDTree extends Node{
 	
 	// Represents direction of the partition
 	private static final boolean VERTICAL = true;
-    private static final boolean HORIZOTAL = false;
+    	private static final boolean HORIZOTAL = false;
 	
 	//private Node root;
 	private int size;
@@ -15,40 +15,104 @@ public class KDTree extends Node{
 		root = null;
 		size = 0;
 	}
-	
-	// Insert 2D points into the tree
+
+	//insert method
 	 public void insert(Point2D p) {
 	        if (p == null) 
-	        		return;
-	        root = insert(root, p);
+	  
+	        //new double[] {x_min, y_min, x_max, y_max}
+	        root = insert(root, p, true, new double[] {0,0,1,1});
 	 }
-	 
-	 private Node insert(Node parent, Point2D p) {
-		 // If parent is null
-		 if (parent == null) {
-			 Node node = new Node();
-			 node.parent = p;
-			 node.left = null;
-			 node.right = null;
+	
+	  private Node insert(Node curNode, Point2D p, boolean evenComponent, double[] coordP) 
+	 {
+		 //if the parent node is null, insert and increment size in the tree
+		 if(curNode == null) 
+		 {
 			 size++;
-			 return node;
+			 return new Node(p, coordP);
 		 }
-		 if (p.getX() < parent.parent.getX()) {
-			 if(parent.left != null) {
-				 parent.left = insert(parent.left, p);
-				 return parent;
-			 } 
-			 parent.left = insert(parent.left, p);
-		 } else {
-				 if (parent.right != null) {
-					 parent.right = insert(parent.right, p);
-					 return parent;
-				 }
-				 parent.right = insert(parent.right, p);
-			 }
+		 
+		 double comp = comparePts(p,curNode,evenComponent);
+		 
+		 //Only deals with x-max coordinate comparisons and specifically left insertions
+		 if(comp < 0 && evenComponent) 
+		 {
+			 coordP[2] = curNode.parent.getX();
+			 curNode.left = insert(curNode.left, p, !evenComponent, coordP);
+		 }
+		 
+		 //y-max coordinate comparisons and odd level (horizontal line)
+		 else if(comp < 0 && !evenComponent) 
+		 {
+			 coordP[3] = curNode.parent.getY();
+			 curNode.left = insert(curNode.left, p, !evenComponent, coordP);
+		 }
+		 
+		 // x-min coordinate comparisons and even level (vertical line)
+		 else if (comp > 0 && evenComponent) 
+		 {
+			 coordP[0] = curNode.parent.getX();
+			 curNode.right = insert(curNode.right, p, !evenComponent, coordP);
+		 }
+		 
+		 // y-min coordinate comparisons and even level (vertical line)
+		 else if (comp > 0 && evenComponent) 
+		 {
+			 coordP[1] = curNode.parent.getY();
+			 curNode.right = insert(curNode.right, p, !evenComponent, coordP);
+		 }
+		 
+		 //handles ties by placing them in the right subtree
+		 else if(!curNode.parent.equals(p)) 
+		 {
+			 curNode.right = insert(curNode.right, p, !evenComponent, coordP);
+		 }
+		return curNode;
+	 }
+	// 	 private Node insert(Node parent, Point2D p) {
+// 		 // If parent is null
+// 		 if (parent == null) {
+// 			 Node node = new Node();
+// 			 node.parent = p;
+// 			 node.left = null;
+// 			 node.right = null;
+// 			 size++;
+// 			 return node;
+// 		 }
+// 		 if (p.getX() < parent.parent.getX()) {
+// 			 if(parent.left != null) {
+// 				 parent.left = insert(parent.left, p);
+// 				 return parent;
+// 			 } 
+// 			 parent.left = insert(parent.left, p);
+// 		 } else {
+// 				 if (parent.right != null) {
+// 					 parent.right = insert(parent.right, p);
+// 					 return parent;
+// 				 }
+// 				 parent.right = insert(parent.right, p);
+// 			 }
 
-		 return parent;      
-		 } 
+// 		 return parent;      
+// 		 } 
+		// Insert 2D points into the tree
+	
+	 /*comparePts determines whether the current level is even. 
+	  * If it's even, the Node's partition line is vertical
+	  * If it is odd, the Node's partition line is horizontal*/
+	 
+	 private double comparePts(Point2D p, Node curNode, boolean evenLvl) 
+	 {
+		 if(evenLvl) 
+		 {
+			 return p.getX()-curNode.parent.getX();
+		 }
+		 else 
+		 {
+			 return p.getY()-curNode.parent.getX();
+		 }
+	 }
 
 
 	//search method to find coordinates
