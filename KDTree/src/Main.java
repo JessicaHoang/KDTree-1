e7948d4598ce17604790b483892600c67c123a3b
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2018-12-16 12:33:14 -0800

    Update Main.java
    We have issues instantiating Point2D

--- KDTree/src/Main.java
+++ KDTree/src/Main.java
@@ -2,9 +2,19 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Rect r = new Rect();
 		r.drawRect();
+		
+		/*create a KDTree*/
+		KdTree kdTree = new KdTree();
+		
+		/*creating point coordinate 1*/
+		double x1 = 50;
+		double y1 = 80;
+		Point2D p = new Point2D(x1,y1);
+		KdTree.insert(p);
+		
+		/*Testing Search Method*/
 
 	}
 
